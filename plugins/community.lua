b46f429a6091e31b17e7d67d065c3498933a65e4
MaoX_Yu <maoxytz@163.com>
2023-12-14 23:15:07 +0800

    feat: add community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,25 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.pack.python-ruff" },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function(_, opts)
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      if require("user.utils").is_win() then
+        path = path .. "\\venv\\Scripts\\python.exe"
+      else
+        path = path .. "/venv/bin/python"
+      end
+      require("dap-python").setup(path, opts)
+    end,
+  },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.split-and-window.edgy-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
 }
