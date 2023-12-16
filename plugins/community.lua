2baf454ef8ff73845e29df52237e451d974a2ccb
MaoX_Yu <maoxytz@163.com>
2023-12-16 21:25:27 +0800

    feat: add plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,8 +4,8 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   {
     "catppuccin/nvim",
@@ -16,7 +16,43 @@ return {
       return opts
     end,
   },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.mini-splitjoin" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.suda-vim" },
+  { import = "astrocommunity.editing-support.telescope-undo-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.yanky-nvim" },
+  {
+    "gbprod/yanky.nvim",
+    optional = true,
+    keys = function(_, keys)
+      keys = vim.list_extend(keys, {
+        {
+          "<leader>P",
+          function() require("telescope").extensions.yank_history.yank_history {} end,
+          desc = "Open Yank History",
+        },
+      })
+    end,
+  },
   { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.nvim-spider" },
+  {
+    "chrisgrieser/nvim-spider",
+    optional = true,
+    opts = function(_, opts)
+      opts.skipInsignificantPunctuation = false
+      return opts
+    end,
+  },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.python-ruff" },
   {
     "mfussenegger/nvim-dap-python",
@@ -31,9 +67,13 @@ return {
       require("dap-python").setup(path, opts)
     end,
   },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
   { import = "astrocommunity.split-and-window.edgy-nvim" },
+  { import = "astrocommunity.test.neotest" },
   { import = "astrocommunity.utility.noice-nvim" },
   {
     "folke/noice.nvim",
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,36 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "dstein64/nvim-scrollview",
+    event = "VeryLazy",
+    config = function()
+      require("scrollview").setup {
+        excluded_filetypes = { "alpha", "dashboard", "neo-tree" },
+        current_only = true,
+        column = 1,
+        signs_on_startup = { "all" },
+      }
+    end,
+  },
+  {
+    "VidocqH/lsp-lens.nvim",
+    event = "LspAttach",
+    opts = {
+      enable = true,
+      include_declaration = false, -- Reference include declaration
+      sections = { -- Enable / Disable specific request
+        definition = false,
+        references = true,
+        implements = true,
+      },
+      ignore_filetype = {
+        "prisma",
+      },
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
